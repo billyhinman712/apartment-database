aeaafd14e6f1263acbfc63e7a1d6b405d7ff5657
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-08-14 08:40:30 -0700

    created quries

--- starter_code/part-3.sql
+++ starter_code/part-3.sql
@@ -1,8 +1,34 @@
 --- Answers for Questions
 
---- 1.
+--- 1.SELECT * FROM owner;
 
---- 2. 
+--- 2. SELECT name FROM owner;
+
+--- 3. SELECT 'age' FROM 'owner' ORDER BY age ASC;
+
+--- 4. SELECT name FROM owner WHERE NAME = 'Donald';
+
+--- 5. SELECT age FROM owner WHERE age> 30; 
+
+--- 6. SELECT name FROM owner WHERE name LIKE 'E%';
+
+--- 7. UPDATE owner SET age = 30 WHERE name = 'Jane';
+
+--- 8. UPDATE owner SET name ='Janet' WHERE id=3;
+
+--- 9. DELETE FROM owner WHERE name='Janet';
+
+--- 10. SELECT name FROM owner LIMIT 3;
+
+--- 11. SELECT * FROM owner WHERE name LIKE '%a%';
+
+--- 12. SELECT * FROM property WHERE name !='Achstone' AND id NOT IN (3,5); 
+
+--- 13. SELECT age FROM owner ORDER BY age DESC LIMIT /;
+
+--- 14. SELECT age FROM owner WHERE age < 30 AND name LIKE '%o%' ORDER BY age DESC LIMIT /;
+
+--- 15. SELECT count(unit) FROM property WHERE owner_id BETWEEN 1 AND 3;
 
 --- etc...
 
